d7d752783a6753eea0da6a2ed9bed79bb51588f3
Alejandro Montepeque <jonathanmontepeque@gmail.com>
2023-06-16 07:34:42 -0600

    PROYECTO TERMINADO

--- proyecto_emergencia.py
+++ proyecto_emergencia.py
@@ -18,6 +18,8 @@ while opcion !=5:
                 self.nombre = nombre
                 self.saldo = saldo
                 self.cuenta = cuenta
+                self.retiro_count = 0
+                self.deposito_count = 0
 
             def __str__(self):
                 return f"Código: {self.codigo}\nNombre: {self.nombre}\nSaldo: {self.saldo}\nNumero de Cuenta: {self.cuenta}\n-------------------"
@@ -77,12 +79,13 @@ while opcion !=5:
             print("3. Salir.")
             opcionExt = int(input("Ingrese la acción que desea tomar: "))
             if opcionExt == 1:
-                num_cuenta = input("Ingrese el número de cuenta al que desea depositar: ")
+                num_cuenta = input("Ingrese su número de cuenta: ")
                 if int(num_cuenta) in listaGlobal:
                     cliente = listaGlobal[int(num_cuenta)]
                     ingreso = float(input("¿Cuánto dinero deseas depositar?: "))
                     if ingreso >= 0:
                         cliente.saldo += ingreso
+                        cliente.deposito_count += 1
                         print(f"Tu saldo total es de {cliente.saldo:.2f}")
                     else:
                         print("La cantidad debe ser mayor a 0")
@@ -94,14 +97,21 @@ while opcion !=5:
                     cliente = listaGlobal[int(num_cuenta)]
                     egreso = float(input("¿Cuánto dinero deseas retirar?: "))
                     if cliente.saldo - egreso < 0:
+                        cliente.retiro_count += 1
                         print(f"No tienes saldo suficiente en tu cuenta. Tu saldo total es de {cliente.saldo:.2f}")
                     else:
+                        cliente.saldo -= egreso
                         cliente.saldo -= egreso
                         print(f"Tu saldo total es de {cliente.saldo:.2f}")
                 else:
                     print("Cliente no encontrado.")
     elif opcion==3:
-        print("3")
+        print("Contadores de retiros y depósitos:")
+        for cliente in listaGlobal.values():
+            print(f"Número de cuenta: {cliente.cuenta}")
+            print(f"Contador de retiros: {cliente.retiro_count}")
+            print(f"Contador de depósitos: {cliente.deposito_count}")
+            print("-------------------")
     elif opcion==4:
         print("Clientes registrados:")
         for cliente in listaGlobal.values():
@@ -111,21 +121,3 @@ while opcion !=5:
         break
     else:
         print("Opcion no valida")
-        transacciones = [
-    {"tipo": "Retiro", "monto": 100},
-    {"tipo": "Depósito", "monto": 200},
-    {"tipo": "Retiro", "monto": 50},
-    {"tipo": "Depósito", "monto": 300}
-]
-
-# Mostrar lista de retiros
-print("Retiros:")
-for transaccion in transacciones:
-    if transaccion["tipo"] == "Retiro":
-        print(f"Monto: {transaccion['monto']}")
-
-# Mostrar lista de depósitos
-print("\nDepósitos:")
-for transaccion in transacciones:
-    if transaccion["tipo"] == "Depósito":
-        print(f"Monto: {transaccion['monto']}")
